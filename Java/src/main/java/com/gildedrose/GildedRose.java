41e23448185f45ddf3ca13512c353181cfff1379
Bart Vandezande <8274691+bartvdz@users.noreply.github.com>
2025-11-08 11:14:19 +0100

    Convert quality and sellIn determination to dedicated methods
    Using the TypedItem we can easily describe the different logic paths for each type for determining quality and sellIn values.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,13 @@ class GildedRose {
     private static final String LABEL_BACKSTAGE_PASSES = "BACKSTAGE PASSES";
     private static final String LABEL_AGED_BRIE = "AGED BRIE";
     private static final String LABEL_SULFURAS = "SULFURAS";
+
+    private static final int NORMAL_DEGRADATION = -1;
+    private static final int AGED_BRIE_DEGRADATION = 1;
+    private static final int BACKSTAGE_PASS_BASE_DEGRADATION = 1;
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,53 +19,112 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            TypedItem typedItem = new TypedItem(item);
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+            item.quality = getNextQuality(typedItem);
+            // Only update SellIn after quality has been processed, as it influences the quality determination
+            item.sellIn = getNextSellIn(typedItem);
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+//            if (!item.name.equals("Aged Brie")
+//                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                if (item.quality > 0) {
+//                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                        item.quality = item.quality - 1;
+//                    }
+//                }
+//            } else {
+//                if (item.quality < 50) {
+//                    item.quality = item.quality + 1;
+//
+//                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (item.sellIn < 11) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1;
+//                            }
+//                        }
+//
+//                        if (item.sellIn < 6) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                item.sellIn = item.sellIn - 1;
+//            }
+//
+//            if (item.sellIn < 0) {
+//                if (!item.name.equals("Aged Brie")) {
+//                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (item.quality > 0) {
+//                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                                item.quality = item.quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        item.quality = 0;
+//                    }
+//                } else {
+//                    if (item.quality < 50) {
+//                        item.quality = item.quality + 1;
+//                    }
+//                }
+//            }
+        }
+    }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+    private int getNextQuality(TypedItem typedItem) {
+        Item item = typedItem.item();
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+        int passedSellInMultiplier = item.sellIn <= 0 ? 2 : 1;
+
+        int newQuality = switch (typedItem.type()) {
+            case NORMAL -> item.quality + NORMAL_DEGRADATION * passedSellInMultiplier;
+            case AGED_BRIE -> item.quality + AGED_BRIE_DEGRADATION * passedSellInMultiplier;
+            case BACKSTAGE_PASS -> getNextBackStageQuality(item);
+            case SULFURAS -> item.quality;
+        };
+
+        if (typedItem.type() == Type.SULFURAS) {
+            return newQuality;
+        }
+
+        return Integer.min(Integer.max(MIN_QUALITY, newQuality), MAX_QUALITY);
+    }
+
+    private int getNextBackStageQuality(Item item) {
+        if (item.sellIn <= 0) {
+            return 0;
+        } else if (item.sellIn <= 5) {
+            return item.quality + BACKSTAGE_PASS_BASE_DEGRADATION * 3;
+        } else if (item.sellIn <= 10) {
+            return item.quality + BACKSTAGE_PASS_BASE_DEGRADATION * 2;
+        } else {
+            return item.quality + BACKSTAGE_PASS_BASE_DEGRADATION;
+        }
+
+        // TODO: Use primitive type in pattern switching when it gets out of preview.
+        //       This makes it even easier to follow and compiler checks if all cases are covered.
+        //
+        // return switch (item.sellIn) {
+        //     case int sellIn when sellIn <= 0 -> 0;
+        //     case int sellIn when sellIn <= 5 -> item.quality + NORMAL_DEGRADATION * 3;
+        //     case int sellIn when sellIn <= 10 -> item.quality + NORMAL_DEGRADATION * 2;
+        //     case int _ -> item.quality + NORMAL_DEGRADATION;
+        // };
+    }
+
+    private int getNextSellIn(TypedItem typedItem) {
+        if (typedItem.type() == Type.SULFURAS) {
+            return typedItem.item().sellIn;
+        }
+
+        return typedItem.item().sellIn - 1;
+    }
+
     /**
      * Convenience record, that can determine type once and group it with original item.
      *
