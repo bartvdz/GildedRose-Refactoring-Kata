cdbdf6584c42c9ce7e028abcbfea17c77c68b344
Bart Vandezande <8274691+bartvdz@users.noreply.github.com>
2025-11-08 11:09:59 +0100

    Introduce TypedItem
     * Convenience record that enhances an Item with its type
     * Type is also determined only once and in a single place

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String LABEL_BACKSTAGE_PASSES = "BACKSTAGE PASSES";
+    private static final String LABEL_AGED_BRIE = "AGED BRIE";
+    private static final String LABEL_SULFURAS = "SULFURAS";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -56,7 +59,49 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
-            }
+    /**
+     * Convenience record, that can determine type once and group it with original item.
+     *
+     * @param item Item that is to be processed
+     * @param type Item type matching the provided item
+     */
+    private record TypedItem(Item item, Type type) {
+        /**
+         * Convenience record that can determine type of provide item itself.
+         *
+         * @param item Item that is to be processed
+         */
+        private TypedItem(Item item) {
+            this(item, getItemType(item));
         }
+
+        private static Type getItemType(Item item) {
+            // Raising string to uppercase for ease of case-insensitive string comparison
+            String upperCaseName = item.name.toUpperCase();
+
+            if (upperCaseName.contains(LABEL_BACKSTAGE_PASSES)) {
+                return Type.BACKSTAGE_PASS;
+            }
+
+            if (upperCaseName.contains(LABEL_AGED_BRIE)) {
+                return Type.AGED_BRIE;
+            }
+
+            if (upperCaseName.contains(LABEL_SULFURAS)) {
+                return Type.SULFURAS;
+            }
+
+            return Type.NORMAL;
+        }
+    }
+
+    /**
+     * All the different known item types
+     */
+    private enum Type {
+        NORMAL,
+        AGED_BRIE,
+        BACKSTAGE_PASS,
+        SULFURAS
     }
 }
