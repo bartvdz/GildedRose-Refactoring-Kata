e72e1456c90cc09b9080068a5e7b062b658854f8
Bart Vandezande <8274691+bartvdz@users.noreply.github.com>
2025-11-08 11:16:16 +0100

    Removing old updateQuality code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -24,55 +24,6 @@ class GildedRose {
             item.quality = getNextQuality(typedItem);
             // Only update SellIn after quality has been processed, as it influences the quality determination
             item.sellIn = getNextSellIn(typedItem);
-
-//            if (!item.name.equals("Aged Brie")
-//                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                if (item.quality > 0) {
-//                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-//                        item.quality = item.quality - 1;
-//                    }
-//                }
-//            } else {
-//                if (item.quality < 50) {
-//                    item.quality = item.quality + 1;
-//
-//                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                        if (item.sellIn < 11) {
-//                            if (item.quality < 50) {
-//                                item.quality = item.quality + 1;
-//                            }
-//                        }
-//
-//                        if (item.sellIn < 6) {
-//                            if (item.quality < 50) {
-//                                item.quality = item.quality + 1;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-//                item.sellIn = item.sellIn - 1;
-//            }
-//
-//            if (item.sellIn < 0) {
-//                if (!item.name.equals("Aged Brie")) {
-//                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                        if (item.quality > 0) {
-//                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-//                                item.quality = item.quality - 1;
-//                            }
-//                        }
-//                    } else {
-//                        item.quality = 0;
-//                    }
-//                } else {
-//                    if (item.quality < 50) {
-//                        item.quality = item.quality + 1;
-//                    }
-//                }
-//            }
         }
     }
 
