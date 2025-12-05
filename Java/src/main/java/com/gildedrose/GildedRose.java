7f90ef8d28a32c69608c7107e962dd5366e77f75
Bart Vandezande <8274691+bartvdz@users.noreply.github.com>
2025-12-05 12:49:11 +0100

    GildedRose uses new sell in service to calculate new sell in value

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,32 +4,39 @@ import com.gildedrose.domain.model.item.ItemCategory;
 import com.gildedrose.domain.service.ItemCategoryResolverService;
 import com.gildedrose.domain.service.quality.QualityService;
 import com.gildedrose.domain.service.quality.QualityServiceFactory;
+import com.gildedrose.domain.service.sellin.SellInService;
+import com.gildedrose.domain.service.sellin.SellInService;
+import com.gildedrose.domain.service.sellin.SellInServiceFactory;
 
 class GildedRose {
     private final Item[] items;
     private final ItemCategoryResolverService itemCategoryResolverService;
     private final QualityServiceFactory qualityServiceFactory;
+    private final SellInServiceFactory sellInServiceFactory;
 
     public GildedRose(Item[] items) {
         this.items = items;
 
         itemCategoryResolverService = new ItemCategoryResolverService();
         qualityServiceFactory = new QualityServiceFactory();
+        sellInServiceFactory = new SellInServiceFactory();
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItemQuality(item);
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            ItemCategory category = itemCategoryResolverService.resolve(item);
+            updateItemQuality(item, category);
+            updateItemSellIn(item, category);
         }
     }
 
-    private void updateItemQuality(Item item) {
-        ItemCategory category = itemCategoryResolverService.resolve(item);
+    private void updateItemQuality(Item item, ItemCategory category) {
         QualityService qualityService = qualityServiceFactory.create(category);
         item.quality = qualityService.updateQuality(item).quality;
+    }
+
+    private void updateItemSellIn(Item item, ItemCategory category) {
+        SellInService sellInService = sellInServiceFactory.create(category);
+        item.sellIn = sellInService.updateSellIn(item).sellIn;
     }
 }
